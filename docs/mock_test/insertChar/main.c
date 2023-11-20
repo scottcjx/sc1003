d3e174afc32251c067e7844ac6e2646532007463
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2023-11-20 12:53:09 +0800

    added mock test

--- docs/mock_test/insertChar/main.c
+++ docs/mock_test/insertChar/main.c
@@ -1,11 +1,21 @@
-# insertChar
-
-[Read the Brief](./insertChar.pdf)
-
-
-## Contents
-
-``` c
+#include <stdio.h>
+#include <string.h>
+void insertChar(char *str1, char *str2, char ch);
+int main()  
+{
+   char a[80],b[80];
+   char ch, *p;
+    
+   printf("Enter a string: \n");   
+   fgets(a, 80, stdin);
+   if (p=strchr(a,'\n')) *p = '\0';  
+   printf("Enter a character to be inserted: \n");   
+   ch = getchar();
+   insertChar(a,b,ch);
+   printf("insertChar(): ");   
+   puts(b);      
+   return 0;
+}
 void insertChar(char *str1, char *str2, char ch)
 {
   int len = strlen(str1);
@@ -27,7 +37,6 @@ void insertChar(char *str1, char *str2, char ch)
           j++;
       }
   }
-
+  
   strcpy(str2, tmpStr);
 }
-```
